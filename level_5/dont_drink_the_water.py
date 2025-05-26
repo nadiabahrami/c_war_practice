4a0460c07fdf2938dbb6d982ff21a731b3c739a1
Nadia Bahrami <nadia.bahrami@gmail.com>
2025-05-26 10:05:05 -0700

    add spacing

--- level_5/dont_drink_the_water.py
+++ level_5/dont_drink_the_water.py
@@ -22,10 +22,12 @@ Given a two-dimensional array representation of a glass of mixed liquids, sort t
  ['W', 'W', 'O', 'W'],  =>    ['A','W','W','W']
  ['H', 'H', 'O', 'O']         ['W','H','H','H']
  ]                           ]
+ 
  input=[[]] output=[[]]
  input=[['A', 'H', 'W', 'O'],['W', 'W', 'O', 'W'],['H', 'H', 'O', 'O']] output= [['O','O','O','O'],['A','W','W','W'],['W','H','H','H']]
  input=[['A', 'H'],['W', 'O'],['W', 'W'],['H', 'H']] ouput=[['O', 'A'],['W', 'W'],['W', 'H],['H', 'H']]
-The glass representation may be larger or smaller. If a liquid doesn't fill a row, it floats to the top and to the left.
+
+ The glass representation may be larger or smaller. If a liquid doesn't fill a row, it floats to the top and to the left.
 """
 
 def separate_liquids(glass):
